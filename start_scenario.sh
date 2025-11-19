1b18136711470da4b19be8554d3eb767b5af27ad
tbo.reflexe <borsenberger@reflexe.fr>
2025-11-19 17:20:04 +0100

    CR LF

--- start_scenario.sh
+++ start_scenario.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-rsync -vP -e "ssh -p 2002"  --filter="+ *.tar" --filter="- *" reflexe@jenkins.reflexe.fr:/home/reflexe/scenario/sofime_scenario_001.tar podman-postgres/sofime_scenario_001.tar
+rsync -vP -e "ssh -p 2002"  --filter="+ *.tar" --filter="- *" reflexe@jenkins.reflexe.fr:/home/reflexe/scenario/sofime_scenario_001.tar podman-postgres/sofime_scenario_001.tar 
 podman-compose up 
  
